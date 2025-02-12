00d8d9cfdfe3180f22d758180c9a5092293b4e2c
loayza1312 james <loayza.james@iisgalvanimi.edu.it>
2025-02-12 13:03:12 +0000

    punto2

---
+++ loayza-angular/src/app/primo-componente/primo-componente.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-primo-componente',
+  imports: [],
+  templateUrl: './primo-componente.component.html',
+  styleUrl: './primo-componente.component.css'
+})
+export class PrimoComponenteComponent {
+
+}
